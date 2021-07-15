351fec19637e89d5629aaba69ae51999acd8ecc3
friskaim7 <friska.intan2001@gmail.com>
2021-07-15 10:21:59 +0700

    refactoring

--- api/controllers/discussionController.js
+++ api/controllers/discussionController.js
@@ -2,34 +2,68 @@
 
 var discussionModel = require("../models/discussionModel");
 
-exports.createDiscussion = function (req, res) {
-    let newDiscussion = discussionModel.createNewDiscussion(req.body);
+module.exports = {
+    createDiscussion : function (req, res) {
+        let newDiscussion = discussionModel.createNewDiscussion(req.body);
+        
+        let db_connect = discussionModel.connectDb();
     
-    let db_connect = discussionModel.connectDb();
+        db_connect.insertOne(newDiscussion, function (err, discussion) {
+            if (err) {
+                return res.status(400).send({
+                    message: err
+                })
+            } else {
+                return res.status(200).send({
+                    message: "Discussion created successfully"
+                });
+            }
+        });
+    },
 
-    db_connect.insertOne(newDiscussion, function (err, post) {
-        if (err) {
-            return res.status(400).send({
-                message: err
-            })
-        } else {
-            return res.status(200).send({
-                message: "Discussion created successfully"
-            });
-        }
-    });
+    getAllDiscussion : function(req, res) {
+        let db_connect = discussionModel.connectDb();
+      
+        db_connect.find({}).toArray(function(err, discussion) {
+            if (err) {
+                return res.status(400).send({
+                    message:err
+                })
+            } else {
+                return res.status(200).send(discussion);
+            }
+        })
+    }
 }
 
-exports.getAllDiscussion = function(req, res) {
-    let db_connect = discussionModel.connectDb();
+// exports.createDiscussion = function (req, res) {
+//     let newDiscussion = discussionModel.createNewDiscussion(req.body);
+    
+//     let db_connect = discussionModel.connectDb();
+
+//     db_connect.insertOne(newDiscussion, function (err, post) {
+//         if (err) {
+//             return res.status(400).send({
+//                 message: err
+//             })
+//         } else {
+//             return res.status(200).send({
+//                 message: "Discussion created successfully"
+//             });
+//         }
+//     });
+// }
+
+// exports.getAllDiscussion = function(req, res) {
+//     let db_connect = discussionModel.connectDb();
   
-    db_connect.find({}).toArray(function(err, discussion) {
-      if (err) {
-        return res.status(400).send({
-          message:err
-        })
-      } else {
-        return res.status(200).send(discussion);
-      }
-    })
-  }
+//     db_connect.find({}).toArray(function(err, discussion) {
+//         if (err) {
+//             return res.status(400).send({
+//                 message:err
+//             })
+//         } else {
+//             return res.status(200).send(discussion);
+//         }
+//     })
+// }
