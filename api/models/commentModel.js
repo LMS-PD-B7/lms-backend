1b44cd091367c5d8b025616310e135310ccd11f7
metardn <54663326+metardn@users.noreply.github.com>
2021-07-21 14:22:16 +0700

    Create commentModel.js

---
+++ api/models/commentModel.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var dbo = require('../../db/connection');
+
+module.exports = {
+    createNewComment: function (comment, account) {
+        let newComment = {
+            email: account.email,
+            date:new Date(),
+            content: comment.content
+        }
+
+        return newComment;
+    },
+
+    connectDb: function () {
+        let db = dbo.getDb();
+
+        if (db === undefined) {
+            return null;
+        }
+
+        return db.collection('comment');
+    }
+
+}
