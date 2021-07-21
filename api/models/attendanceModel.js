4401a5b79191afd889efd6fbb15dc7e98cc9b3f8
kayisshal <61397531+kayisshal@users.noreply.github.com>
2021-07-21 17:39:52 +0700

    Revert "update attendance Controler dan Model"
    This reverts commit cf44cef39bbe19cccfbb370638a093b22a110c25.

--- api/models/attendanceModel.js
+++ api/models/attendanceModel.js
@@ -1,31 +1,15 @@
 'use strict';
 
-var dbo = require('../../db/connection'),
-    { ObjectID } = require('mongodb');
+var dbo = require('../../db/connection');
 
 module.exports = {
-    createNewAttendance: function (attendance, account, course) {
+    createNewAttendance: function (attendance) {
         const newAttendance = {
-            id_course: new ObjectID(course._id),
-            maker_emal: new ObjectID(account.email),
-            title: attendance.title,
-            description: discussion.description,
-            date: new Date(),
-            late_limit: attendance.late_limit,
-            deadline: new Date()
+            
         }
 
         return newAttendance;
     },
-
-    updateAttendance: function (attendance) {
-        return {
-            title: attendance.title,
-            description: attendance.description,
-            date: attendance.date
-          }
-    },
-
 
     connectDb: function () {
         let db = dbo.getDb();
