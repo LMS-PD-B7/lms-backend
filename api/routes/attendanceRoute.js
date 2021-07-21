e7db28370c57dc3c90536580777bc0caeaba1547
kayisshal <61397531+kayisshal@users.noreply.github.com>
2021-07-21 21:05:08 +0700

    Revert "Merge branch 'main' of https://github.com/LMS-PD-B7/lms-backend into main"
    This reverts commit 7b64e8177d7833182969af33623a01972aaeff97, reversing
    changes made to c04b4394c8c35dd1ba0e3e022fa2530d4b3cc28e.

--- api/routes/attendanceRoute.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-    let attendanceHandler = require("../controllers/attendanceController");
-
-    app.route('/attendance')
-        .post(attendanceHandler.createAttendance)
-        .get(attendanceHandler.getAllAttendance);
-}
