4f7e7db80942987b718265508025d3b14dcfaaed
kayisshal <61397531+kayisshal@users.noreply.github.com>
2021-07-22 14:26:34 +0700

    delete attendance done

--- api/controllers/attendanceController.js
+++ api/controllers/attendanceController.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var attendanceModel = require("../models/attendanceModel");
-const courseModel = require("../models/courseModel");
-const accountModel = require("../models/accountModel");
+var courseModel = require("../models/courseModel");
+var accountModel = require("../models/accountModel");
+var submissionModel = require("../models/submissionModel");
 
 exports.createAttendance = async function (req, res) {
     if (req.account) {
@@ -72,5 +73,34 @@ exports.deleteAttendance = async function(req, res) {
   } else {
     return res.status(401).send({ message: 'Invalid token' });
   }
+}
+
+exports.updateAttendance = async function(req, res) {
+  if (req.account) {
+    let db_connect = attendanceModel.connectDb();
+    let course_db_connect = courseModel.connectDb();
+    let acc_db_connect = accountModel.connectDb();
+
+    let attendance = await db_connect.findOne({ _id: new ObjectID(req.params.id) });
+    let course = await course_db_connect.findOne({ _id: new ObjectID(req.params.id) });
+
+    if (!course || course.teacher[0] !== req.account.email) {
+        return res.status(400).send({ message: "Not authorized" });
+    }
+
+    const query = { _id: new ObjectID(req.params.id) };
+
+    db_connect.updateOne(query, values, function (err, attendance) {
+      if (err) {
+          return res.status(400).send({ message: err })
+      }
+      return res.status(200).json({ message: 'Attendance detail Updated' });
+    });
+  } else {
+    return res.status(401).send({ message: 'Invalid token' });
+  }
+}
+
+exports.fillAttendance = function(req, res) {
 
 }
--- api/routes/attendanceRoute.js
+++ api/routes/attendanceRoute.js
@@ -1,12 +1,13 @@
 'use strict';
 
 module.exports = function (app) {
-    let attendanceHandler = require("../controllers/attendanceController");
+    let attendanceHandler = require("../controllers/attendanceController"),
+        accountHandler = require("../controllers/accountController");
 
     app.route('/attendance')
-        .post(attendanceHandler.createDiscussion)
-        .get(attendanceHandler.getAllDiscussion);
-    app.route('/attendance/update')
-        .put(attendanceHandler.update);
+        .post(accountHandler.loginRequired, attendanceHandler.createAttendance)
+        .get(attendanceHandler.getAllAttendances);
+    app.route('/attendance/delete')
+        .put(attendanceHandler.deleteAttendance);
    // app.route('')
 }
