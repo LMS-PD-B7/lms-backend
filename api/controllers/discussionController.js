a32da94ba628801b3873fd7dd2cc9f77df7937b4
friskaim7 <friska.intan2001@gmail.com>
2021-08-02 15:42:41 +0700

    refactoring : delete inactive code

--- api/controllers/discussionController.js
+++ api/controllers/discussionController.js
@@ -25,40 +25,6 @@ exports.updateDiscussionList = async function (req, res, discuss) {
     }
 }
 
-// exports.deleteDiscussion = async function (req, res) {
-//     let db_connect = courseModel.connectDb();
-//     let course = await db_connect.findOne({ _id: new ObjectID(req.params.id) });
-//     if (course) {
-//         const query = { _id: new ObjectID(req.params.id) };
-//         const values = {
-//             $pull: {
-//                 discussions: new ObjectID(req.params.id_discussion)
-//             }
-//         };
-
-//         db_connect.updateOne(query, values, {}, function (err, account) {
-//             if (err) {
-//                 return res.status(400).send({ message: err })
-//             }
-//             let discuss_db_connect = discussionModel.connectDb();
-//             // let discussion = discuss_db_connect.findOne({_id: new ObjectID(id_discussion)})
-
-//             const query = { _id: new ObjectID(req.params.id) };
-//             discuss_db_connect.remove(query, 1, function (err, discussion) {
-//                 if (err) {
-//                     return res.status(400).send({ message: err });
-//                 } else {
-//                     return res.status(200).send({ message: 'Discussion deleted' });
-//                 }
-//             })
-
-//             return res.status(200).json({ message: 'User Updated' });
-//         });
-//     } else {
-//         return res.status(401).send({ message: 'Course not found' });
-//     }
-// }
-
 exports.deleteDiscussion = async function (req, res) {
     let db_connect = discussionModel.connectDb();
     let discussion = await db_connect.findOne({ _id: new ObjectID(req.params.id) });
